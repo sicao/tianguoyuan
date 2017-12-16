477b9e6db8fcbb17c840ace44f96a1a7585053f2
sunmeng <time_sunm@163.com>
2017-12-16 15:14:48 +0800

    修复bug

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -8,7 +8,7 @@
 				<li v-for="(item,index) in classOneGroup" :key="item.id" @click="show(index)">{{item.name}}</li>
 			</ul>
 			<div class="item" >
-				<h2>{{childrenList[itemIndex].class2Name.name}}<span class="more">全部 <i>></i></span></h2>
+				<h2>{{name}}<span class="more">全部 <i>></i></span></h2>
 				<div class="items">
 					<dl v-for="item in class3Group" :key="item.id">
 						<dt><img :src="item.class_photo"></dt>
@@ -26,7 +26,8 @@
 				classOneGroup:[],
 				childrenList:[],
 				itemIndex: 0,
-				class3Group: []
+				class3Group: [],
+				name: '果园优选'
 			}
 		},
 		created(){		
@@ -36,14 +37,13 @@
 			this.classOneGroup=res.data.data.classOneGroup;
 			this.childrenList=res.data.data.childrenList;
 			this.class3Group = this.childrenList[this.itemIndex].class3Group;
-			console.log(this.childrenList);
 			});
 		},
 		methods:{
 			show(index){
 				this.itemIndex = index;
 				this.class3Group = this.childrenList[this.itemIndex].class3Group;
-				console.log(this.class3Group);
+				this.name = this.childrenList[this.itemIndex].class2Name.name
 			}
 		}
 	}
